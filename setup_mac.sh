04c7611b1f2ff46512694f081f732a7475c94d72
Vairavan Laxman <vayiravan@gmail.com>
2015-10-08 11:41:09 -0700

    Sublime Text 3 link, vi plugin manager vim-plug support

--- setup_mac.sh
+++ setup_mac.sh
@@ -5,3 +5,11 @@ chflags nohidden ~/Library/
 
 # Link Sublime executable to bin directory for temrinal access
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl  # Sublime 3
+
+# Install vim-plug to manage vim plugins
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Now you need to install all plugins within vim using vim-plug
+# Launch vim, then type:
+:PlugInstall

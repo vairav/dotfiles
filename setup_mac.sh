894e037cd179eefcdb1bc7bced7afa162ae5e6d2
Vairavan Laxman <vairavan.laxman@privoro.com>
2018-11-08 04:46:51 -0500

    MAC setup

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,3 +1,35 @@
+# Install zsh
+brew update
+brew upgrade
+
+brew install zsh zsh-completions
+
+# Check what is the current default shell
+dscl . -read /Users/$USER UserShell
+
+# Check if the brew zsh is installed properly
+ls -la /usr/local/bin/zs*
+
+# Set ZSH as the default shell
+sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
+
+# Confirm that the homebrew zsh is being used
+dscl . -read /Users/$USER UserShell
+
+# Setup Powerline fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+# Install/Copy over fonts to the fonts folder in Mac OSX
+cd fonts
+./install.sh
+# Remove the git cloned folder as it is not needed any more
+cd ..
+rm -rf fonts
+
+# Install Powerline9k ZSH plugin
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# Change the theme in .zshrc to the one below:
+# ZSH_THEME="powerlevel9k/powerlevel9k"
+
 # Mac OSX related fixes
 
 # Show User's library folder all the time

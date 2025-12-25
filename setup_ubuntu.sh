62fbc61595b29ae109a9a7dd7ee5ba215c047aec
Vairav Laxman <vayiravan@gmail.com>
2025-12-25 15:47:00 -0500

    chore: remove old files and deprecated configs
    - Delete .brew, .cask, .screenrc, .gitmodules, .osx
    - Delete ls/ (using eza now, no need for dircolors)
    - Delete setup_mac.sh, setup_ubuntu.sh (replaced by new scripts)
    - Delete old config folders moved to config/ or no longer used:
      alacritty, kitty, htop, i3wm, karabiner, nvim, sublime, vim
    - Clean up old zsh structure (moved to zsh/common, zsh/frameworks)

--- setup_ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Download and install vim-plug plugin manager
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Install all plugins using vim-plug from the .vimrc
-echo "Installing plugins..."
-vim +PlugInstall +qall
